bbfba4bf231d18d042122578c1fac2aa2748e494
Benyomin Walters <chassidus@gmail.com>
2019-08-09 17:10:38 -0500

    added api to server

--- 2.1_Lesson/index.js
+++ 2.1_Lesson/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.listen(port, () => console.log('listening at 3000'));
-
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+	console.log('I got a request');
+	console.log(request.body);
+	const data = request.body;
+
+	fs.appendFile('myLocations.txt', JSON.stringify(data, null, '\t'), (err) => {
+		if (err) throw err;
+		console.log('Saved!');
+	});
+
+	response.json({
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.lng
+	});
+});
