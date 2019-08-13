30574875ac6222cd1cfc867b719dfe2bdbe20c87
Benyomin Walters <chassidus@gmail.com>
2019-08-13 18:04:13 -0500

    Added server with database

--- 2.1_Lesson/index.js
+++ 2.1_Lesson/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 const port = 3000;
 const fs = require('fs');
@@ -7,19 +8,36 @@ app.listen(port, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
-	console.log('I got a request');
-	console.log(request.body);
+	// console.log('I got a request');
+	// console.log(request.body);
+
 	const data = request.body;
+	const timestamp = Date.now();
+	data.timestamp = timestamp;
 
-	fs.appendFile('myLocations.txt', JSON.stringify(data, null, '\t'), (err) => {
-		if (err) throw err;
-		console.log('Saved!');
-	});
+	database.insert(data);
+	response.json(data);
 
-	response.json({
-		status: 'success',
-		latitude: data.lat,
-		longitude: data.lng
+	// Code to manually add data to a txt file.
+	// This was for demo purposes
+	//
+	// fs.appendFile('myLocations.txt', JSON.stringify(data, null, '\t'), (err) => {
+	// 	if (err) throw err;
+	// 	console.log('Saved!');
+	// });
+});
+
+app.get('/api', (request, response) => {
+	database.find({}, (err, data) => {
+		if (err) {
+			console.log(err);
+			response.end();
+			return;
+		}
+		response.json(data);
 	});
 });
